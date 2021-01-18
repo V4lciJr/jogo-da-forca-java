2682a46913dcb78c655ad120660c6ab7b758d940
Valci Júnior <valcijunior19@hotmail.com>
2021-01-18 18:03:53 -0300

    Classe Word Finalizada

--- src/main/java/jogodaforca/game/Game.java
+++ src/main/java/jogodaforca/game/Game.java
@@ -1,9 +1,18 @@
 package jogodaforca.game;
 
+import jogodaforca.core.Word;
+
 public class Game {
 	
 	public void start()
 	{
-		
+		Word word = new Word("Valci");
+		word.hasChar('v');
+		word.hasChar('c');
+		word.hasChar('l');
+		word.hasChar('i');
+		word.hasChar('a');
+		System.out.println(word.discovered());
+		System.out.println(word);
 	}
 }
